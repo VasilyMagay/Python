bc8a0dfef7a590feff688f93a2c22edd61304881
Vasily <33507763+VasilyMagay@users.noreply.github.com>
2017-11-19 10:52:15 +0300

    Add files via upload

---
+++ File_Open/file_open.py
@@ -0,0 +1,51 @@
+def read_recepts(filename):
+  book = {}
+  with open(filename) as f:
+    while True:
+      recept = f.readline()
+      recept = recept.strip()
+      recept = recept.lower()
+      ingredient_num = int(f.readline())
+      if book.get(recept) is None:
+        book[recept] = []
+        for s in range(ingredient_num):
+          product = f.readline()
+          product = product.strip()
+          product_list = product.split(" | ")
+          book[recept].append({'ingridient_name': product_list[0].lower(), 'quantity': int(product_list[1]), 'measure': product_list[2].lower()})
+      else:
+        # Повторный рецепт не добавляем
+        for s in range(ingredient_num):
+          f.readline()
+      if not f.readline():
+        break
+  return book
+
+def get_shop_list_by_dishes(dishes, person_count):
+  shop_list = {}
+  for dish in dishes:
+    for ingridient in cook_book[dish]:
+      new_shop_list_item = dict(ingridient)
+
+      new_shop_list_item['quantity'] *= person_count
+      if new_shop_list_item['ingridient_name'] not in shop_list:
+        shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+      else:
+        shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+  return shop_list
+
+def print_shop_list(shop_list):
+  for shop_list_item in shop_list.values():
+    print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], 
+                            shop_list_item['measure']))
+
+def create_shop_list():
+  person_count = int(input('Введите количество человек: '))
+  dishes = input('Введите блюда в расчете на одного человека (через запятую): ') \
+    .lower().split(', ')
+  shop_list = get_shop_list_by_dishes(dishes, person_count)
+  print_shop_list(shop_list)
+
+cook_book = read_recepts('recepts.txt')
+
+create_shop_list()
